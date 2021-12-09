524419039acaffe745be183dce69065d34350ae8
anupam-tiwari <appleanupam@gmail.com>
2021-12-09 14:37:18 -0500

    added get request

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ app.post('/post-test', (req, res) => {
     res.sendStatus(200);
 });
 
+app.use('/Login', (req, res) => {
+  res.send({
+      token: 'test123'
+  });
+});
+
 app.listen(8080, () => console.log(`Started server at http://localhost:8080!`));
