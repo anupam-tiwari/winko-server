3095ddad6bb3f2ef15ed3fd74160143f8e03deb4
anupam-tiwari <appleanupam@gmail.com>
2022-06-14 13:31:31 +0800

    added cors

--- server.js
+++ server.js
@@ -32,6 +32,19 @@ app.use('/chat', chatRouter)
 
 app.use('/finance', finaceRouter)
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // handle OPTIONS method
+  if ('OPTIONS' == req.method) {
+      return res.sendStatus(200);
+  } else {
+      next();
+  }
+});
+
 app.post('/Number', (req, res) => {
     resbody = req.body.Body.split(',')
     console.log('Got body:', req.body.Body);
